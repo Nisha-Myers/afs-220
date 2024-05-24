7acb533432249e1e97e7d9081c1f0324eb171798
unknown <equiliberalium89@gmail.com>
2024-05-23 23:42:08 -0400

    .

---
+++ cozy_cottage/tests/test_views.py
@@ -0,0 +1,16 @@
+|__<cozy_cottage>
+    |__tests
+        |-- __init__.py
+        |-- test_views.py 
+        
+from django.test import TestCase
+from django.test import Client
+
+
+class PageViewTests(TestCase):
+
+    c = Client()
+
+    def test_page_status_code(self):
+        response = self.c.get('/home')
+        self.assertEquals(response.status_code, 200)
